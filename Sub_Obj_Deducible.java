b722963f5a153798061a50fcc606aeb382c5e220
axaloboapolo <30607583+axaloboapolo@users.noreply.github.com>
2017-10-11 16:07:06 -0500

    Create Sub_Obj_Deducible.java
    se creo el sub objeto deducible de cobertura

---
+++ Sub_Obj_Deducible.java
@@ -0,0 +1,15 @@
+package mx.axa.autos.tarifa.Objetos;
+
+public class Sub_Obj_Deducible {
+	private double deducible;
+
+	public double getDeducible() {
+		return deducible;
+	}
+
+	public void setDeducible(double deducible) {
+		this.deducible = deducible;
+	}
+	
+
+}
