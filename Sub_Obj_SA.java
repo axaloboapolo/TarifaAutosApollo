b015df13dc4219243f8a0ae07457f9cc3ad22f1c
axaloboapolo <30607583+axaloboapolo@users.noreply.github.com>
2017-10-11 16:05:37 -0500

    Create Sub_Obj_SA.java
    sub objeto suma asegurada de cobertura

---
+++ Sub_Obj_SA.java
@@ -0,0 +1,16 @@
+package mx.axa.autos.tarifa.Objetos;
+
+public class Sub_Obj_SA {
+	private double sa;
+
+	public double getSa() {
+		return sa;
+	}
+
+	public void setSa(double sa) {
+		this.sa = sa;
+	}
+
+	
+
+}
