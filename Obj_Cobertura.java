ef58537841dde599982f0b80960720fb0287ebd3
axaloboapolo <30607583+axaloboapolo@users.noreply.github.com>
2017-10-11 16:06:15 -0500

    Create Obj_Cobertura.java
    se creo objeto cobertura

---
+++ Obj_Cobertura.java
@@ -0,0 +1,28 @@
+package mx.axa.autos.tarifa.Objetos;
+
+public class Obj_Cobertura {
+  private String descripcion;
+ private Sub_Obj_Deducible[] deducible;
+ private Sub_Obj_SA[] suma;
+
+public String getDescripcion() {
+	return descripcion;
+}
+public void setDescripcion(String descripcion) {
+	this.descripcion = descripcion;
+}
+public Sub_Obj_Deducible[] getDeducible() {
+	return deducible;
+}
+public void setDeducible(Sub_Obj_Deducible[] deducible) {
+	this.deducible = deducible;
+}
+public Sub_Obj_SA[] getSuma() {
+	return suma;
+}
+public void setSuma(Sub_Obj_SA[] suma) {
+	this.suma = suma;
+}
+ 
+ 
+}
